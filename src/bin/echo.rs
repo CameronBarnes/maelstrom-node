ea887a02f5305113565f2edf3948c270e3551d52
Cameron Barnes <cameron_barnes@outlook.com>
2024-03-20 15:41:01 -0700

    Consolidated shared code and cleaned everything up

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,141 +1,50 @@
-use std::io::{StdoutLock, Write};
+use std::io::StdoutLock;
 
-use anyhow::{Result, Context, bail};
-use maelstrom_node::{Body, Message, Node, main_loop};
-use serde::{Serialize, Deserialize};
+use anyhow::Result;
+use maelstrom_node::{main_loop, Message, Node};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
 pub enum Payload {
-    Init{
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk{},
-    Echo{
-        echo: String,
-    },
-    EchoOk{
-        echo: String,
-    },
-    Error {
-        code: usize,
-        text: Option<String>,
-    }
+    Echo { echo: String },
+    EchoOk { echo: String },
 }
 
 #[derive(Default)]
 struct EchoNode {
-    id: Option<String>,
+    id: String,
     msg_id: usize,
 }
 
 impl Node<Payload> for EchoNode {
+    fn from_init(init: maelstrom_node::Init) -> Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(EchoNode {
+            id: init.node_id,
+            msg_id: 1,
+        })
+    }
+
+    fn next_msg_id(&mut self) -> usize {
+        let out = self.msg_id;
+        self.msg_id += 1;
+        out
+    }
+
+    fn node_id(&self) -> String {
+        self.id.clone()
+    }
+
     fn process_message(&mut self, msg: Message<Payload>, output: &mut StdoutLock) -> Result<()> {
-        match msg.body.payload {
-            Payload::Init { node_id, .. } => {
-                if self.id.is_some() {
-                    let body = Body {
-                        id: Some(self.msg_id),
-                        reply_to: msg.body.id,
-                        payload: Payload::Error {
-                            code: 14,
-                            text: Some(String::from("Recieved Init twice")),
-                        },
-                    };
-                    self.msg_id += 1;
-                    serde_json::to_writer(&mut *output, &Message {
-                        src: String::from("uninitialized"),
-                        dest: msg.src,
-                        body,
-                    })?;
-                    output.write_all(b"\n").context("write trailing newline")?;
-                    bail!("received init twice")
-                } else {
-                    self.id = Some(node_id.clone());
-                    let body = Body {
-                        id: Some(self.msg_id),
-                        reply_to: msg.body.id,
-                        payload: Payload::InitOk {},
-                    };
-                    self.msg_id += 1;
-                    serde_json::to_writer(&mut *output, &Message {
-                        src: node_id.clone(),
-                        dest: msg.src,
-                        body,
-                    })?;
-                    output.write_all(b"\n").context("write trailing newline")?;
-                }
-            }
+        match msg.body.payload.clone() {
             Payload::Echo { echo } => {
-                if let Some(id) = &self.id {
-                    let body = Body {
-                        id: Some(self.msg_id),
-                        reply_to: msg.body.id,
-                        payload: Payload::EchoOk { echo },
-                    };
-                    self.msg_id += 1;
-                    serde_json::to_writer(&mut *output, &Message {
-                        src: id.clone(),
-                        dest: msg.src,
-                        body,
-                    })?;
-                    output.write_all(b"\n").context("write trailing newline")?;
-                } else {
-                    let body = Body {
-                        id: Some(self.msg_id),
-                        reply_to: msg.body.id,
-                        payload: Payload::Error {
-                            code: 11,
-                            text: Some(String::from("Have not yet receieved Init")),
-                        },
-                    };
-                    self.msg_id += 1;
-                    serde_json::to_writer(&mut *output, &Message {
-                        src: String::from("uninitialized"),
-                        dest: msg.src,
-                        body,
-                    })?;
-                    output.write_all(b"\n").context("write trailing newline")?;
-                }
+                self.reply(msg, Payload::EchoOk { echo }).send(output)?;
             }
             Payload::EchoOk { .. } => {}
-            _ => {
-                if let Some(id) = &self.id {
-                    let body = Body {
-                        id: Some(self.msg_id),
-                        reply_to: msg.body.id,
-                        payload: Payload::Error {
-                            code: 10,
-                            text: Some(String::from("unimplemented or not supported")),
-                        },
-                    };
-                    self.msg_id += 1;
-                    serde_json::to_writer(&mut *output, &Message {
-                        src: id.clone(),
-                        dest: msg.src,
-                        body,
-                    })?;
-                    output.write_all(b"\n").context("write trailing newline")?;
-                } else {
-                    let body = Body {
-                        id: Some(self.msg_id),
-                        reply_to: msg.body.id,
-                        payload: Payload::Error {
-                            code: 11,
-                            text: Some(String::from("Have not yet receieved Init")),
-                        },
-                    };
-                    self.msg_id += 1;
-                    serde_json::to_writer(&mut *output, &Message {
-                        src: String::from("uninitialized"),
-                        dest: msg.src,
-                        body,
-                    })?;
-                    output.write_all(b"\n").context("write trailing newline")?;
-                }
-            }
         }
 
         Ok(())
@@ -143,5 +52,5 @@ impl Node<Payload> for EchoNode {
 }
 
 pub fn main() -> Result<()> {
-    main_loop(EchoNode::default())
+    main_loop::<EchoNode, Payload>()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,24 @@
-use std::io::StdoutLock;
+use std::io::{BufRead, StdoutLock, Write};
 
-use anyhow::{Result, Context};
-use serde::{Serialize, Deserialize, de::DeserializeOwned};
+use anyhow::{Context, Result};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Message<Payload> {
     pub src: String,
     pub dest: String,
     pub body: Body<Payload>,
+}
+
+impl<Payload> Message<Payload> {
+    pub fn send(&self, output: &mut impl Write) -> Result<()>
+    where
+        Payload: Serialize,
+    {
+        serde_json::to_writer(&mut *output, self).context("serialize response message")?;
+        output.write_all(b"\n").context("write trailing newline")?;
+        Ok(())
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -23,45 +34,75 @@ pub struct Body<Payload> {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
-pub enum InitPayload {
-    Init{
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk{},
+enum InitPayload {
+    Init(Init),
+    InitOk,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "type")]
-#[serde(rename_all = "snake_case")]
-pub enum ErrorPayload {
-    Error {
-        code: usize,
-        text: Option<String>,
-    }
+pub struct Init {
+    pub node_id: String,
+    pub node_ids: Vec<String>,
 }
 
 pub trait Node<Payload> {
+    fn from_init(init: Init) -> Result<Self>
+    where
+        Self: Sized;
+    fn next_msg_id(&mut self) -> usize;
+    fn node_id(&self) -> String;
     fn process_message(&mut self, msg: Message<Payload>, output: &mut StdoutLock) -> Result<()>;
+    fn reply(&mut self, msg: Message<Payload>, payload: Payload) -> Message<Payload> {
+        let mut body = msg.body;
+        body.reply_to = body.id;
+        body.id = Some(self.next_msg_id());
+        body.payload = payload;
+        Message {
+            src: self.node_id(),
+            dest: msg.src,
+            body,
+        }
+    }
 }
 
-pub fn main_loop<State, Payload>(mut state: State) -> Result<()> where State: Node<Payload>, Payload: DeserializeOwned {
+pub fn main_loop<State, Payload>() -> Result<()>
+where
+    State: Node<Payload>,
+    Payload: DeserializeOwned,
+{
     let stdin = std::io::stdin().lock();
-    let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message<Payload>>();
-
+    let mut stdin = stdin.lines();
     let mut stdout = std::io::stdout().lock();
 
-    for input in inputs {
-        let input = input.context("serialize maelstrom input")?;
+    let init_msg: Message<InitPayload> = serde_json::from_str(
+        &stdin
+            .next()
+            .expect("should receive an init message")
+            .context("failed to read init msg from stdin")?,
+    )
+    .context("init message could not be deserialized")?;
+    let InitPayload::Init(init) = init_msg.body.payload else {
+        panic!("first message should be init");
+    };
+
+    Message {
+        src: init.node_id.clone(),
+        dest: init_msg.src.clone(),
+        body: Body {
+            id: Some(0),
+            reply_to: init_msg.body.id,
+            payload: InitPayload::InitOk,
+        },
+    }
+    .send(&mut stdout)?;
+
+    let mut state: State = Node::from_init(init).context("node init failed")?;
+
+    for line in stdin {
+        let input = line.context("get stdin")?;
+        let input = serde_json::from_str(&input).context("serialize input")?;
         state.process_message(input, &mut stdout)?;
     }
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    
 }
